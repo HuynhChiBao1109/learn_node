c99e7ae74f4341eb12d4665893c896bb5a170107
baoHC <baohc110902@gmail.com>
2023-01-07 08:34:21 +0700

    routing

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
+const e = require('express');
 const http = require('http');
+const url = require('url');
 
 // Server
 const server = http.createServer((req, res) => {
-    res.end("Huynh Chi Bao");
+    const path = req.url;
+
+    if (path === "/" || path === "/overview") {
+        res.end("this is overview");
+    } else if (path === "/product") {
+        res.end ("this is product");
+    } else {
+        res.writeHead(404 , {
+            "Content-type" : "text/html",
+            "my-own-header" : "hello-world"
+        });
+        res.end("<h1>page not found</h1>");
+    }
+
 })
 
 server.listen(8000 , '127.0.0.1', () => {
