5bfd7013671b3da845bb0f5d2c6e50115f7288f1
baoHC <baohc110902@gmail.com>
2023-01-07 10:26:27 +0700

    edit_url_product_html

--- index.js
+++ index.js
@@ -29,20 +29,23 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
 // Server
 const server = http.createServer((req, res) => {
-    const path = req.url;
+    // url
+    const { query, pathname } = url.parse(req.url, true);
     //overview
-    if (path === "/" || path === "/overview") {
+    if (pathname === "/" || pathname === "/overview") {
         res.writeHead(200, { 'Content-type': 'text/html' });
-
         // api_html
         const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
         const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
         res.end(output);
         //product
-    } else if (path === "/product") {
-        res.end("this is product");
+    } else if (pathname === "/product") {
+        res.writeHead(200, { 'Content-type': 'text/html' });
+        const product = dataObj[query.id];
+        const output = replaceTemplate(tempProduct, product);
+        res.end(output);
         //api
-    } else if (path === "/api") {
+    } else if (pathname === "/api") {
         res.writeHead(200, { 'Content-type': 'application/json' });
         res.end(data);
     }
