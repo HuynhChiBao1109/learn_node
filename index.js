59b36ef0b08b19d4c525e0e2d6aefeb93e82ea8f
baoHC <baohc110902@gmail.com>
2023-01-07 08:18:25 +0700

    create_simple_web

--- index.js
+++ index.js
@@ -1,32 +1,8 @@
-const fs = require('fs');
 const http = require('http');
-const url = require ('url');
-//////////////////////////
-// Files
-// Blocking 
-// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
-// console.log(textIn);
 
-// const textOut = `Huynh Chi Bao : ${textIn}`;    
-// fs.writeFileSync('./txt/ouput.txt', textOut);
-// console.log(textOut)
-// Non-Blocking
-// fs.readFile('./txt/start.txt', 'utf-8', (err,data1) => {
-//     fs.readFile('./txt/${data1}.txt', 'utf-8', (err,data2) => {
-//         console.log(data2);
-//     });
-// });
-///////////////////////////////
-/// Server
+// Server
 const server = http.createServer((req, res) => {
-    const path = req.url;
-
-    if (path === '/product') {
-        res.end("This is product");
-    } else if (path === '/api') {
-        res.end('./templates/overview');
-    }
-
+    res.end("Huynh Chi Bao");
 })
 
 server.listen(8000 , '127.0.0.1', () => {
