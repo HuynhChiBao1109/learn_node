d1050bf00c93a5d1e705319112b478b7404df8b5
baoHC <baohc110902@gmail.com>
2023-01-07 08:55:25 +0700

    api_json

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
-const e = require('express');
+const fs = require('fs');
 const http = require('http');
 const url = require('url');
+
+
+//api
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const productData = JSON.parse(data);
 
 // Server
 const server = http.createServer((req, res) => {
     const path = req.url;
-
+    //overview
     if (path === "/" || path === "/overview") {
         res.end("this is overview");
+        //product
     } else if (path === "/product") {
-        res.end ("this is product");
-    } else {
-        res.writeHead(404 , {
-            "Content-type" : "text/html",
-            "my-own-header" : "hello-world"
+        res.end("this is product");
+        //api
+    } else if (path === "/api") {
+        res.writeHead(200, { 'Content-type': 'application/json' })
+        res.end(data); 
+    }
+    // not found 404
+    else {
+        res.writeHead(404, {
+            "Content-type": "text/html",
+            "my-own-header": "hello-world"
         });
         res.end("<h1>page not found</h1>");
     }
 
 })
 
-server.listen(8000 , '127.0.0.1', () => {
+server.listen(8000, '127.0.0.1', () => {
     console.log("hcb");
 })
