e946438f5d021a7a79edbb54d3efb877508ae09b
baoHC <baohc110902@gmail.com>
2023-01-11 11:16:15 +0700

    api_simple

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const express = require('express');
-
+const fs = require('fs');
 const app = express()
 
-app.get('/', (req, res) => {
-    res.status(200).json({
-        message: "Hello",
-        app: "Natour"
-    });
-})
+// app.get('/', (req, res) => {
+//     res.status(200).json({
+//         message: "Hello",
+//         app: "Natour"
+//     });
+// })
 
-app.post('/', (req, res) => {
-    res.send("You can post at endpoint");
+// app.post('/', (req, res) => {
+//     res.send("You can post at endpoint");
+// })
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+)
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: success,
+        result: tours.length,
+        data: {
+
+        }
+    })
 })
 
 const port = 3000;
