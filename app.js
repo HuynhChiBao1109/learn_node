065afa907871580cbd225fda58e1673486c2b588
baoHC <baohc110902@gmail.com>
2023-01-16 00:31:43 +0700

    post_api

--- app.js
+++ app.js
@@ -1,31 +1,45 @@
 const express = require('express');
+const app = express();
+app.use(express.json());
 const fs = require('fs');
-const app = express()
 
-// app.get('/', (req, res) => {
-//     res.status(200).json({
-//         message: "Hello",
-//         app: "Natour"
-//     });
-// })
-
-// app.post('/', (req, res) => {
-//     res.send("You can post at endpoint");
-// })
 const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-)
-app.get('/api/v1/tours', (req, res) => {
+);
+
+app.get('/api/v1/tours/:id', (req, res) => {
+
     res.status(200).json({
-        status: success,
+        status: 'sucess',
         result: tours.length,
         data: {
-
+            tours
         }
     })
 })
 
+app.post('/api/v1/tours', (req, res) => {
+    // console.log(req.body);
+
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId }, req.body);
+
+    tours.push(newTour);
+
+    fs.writeFile(
+        `${__dirname}/dev-data/data/tours-simple.json`,
+        JSON.stringify(tours),
+        err => {
+            res.status(201).json({
+                status: 'sucess',
+                data: {
+                    tours: newTour
+                }
+            })
+        })
+})
+
 const port = 3000;
 app.listen(port, () => {
-    console.log(`App running on port ${port}`);
+    console.log(`App running on ${port}`);
 })
